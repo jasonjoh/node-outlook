d1eb898239f511f228e19040b3c28a1f6b42dff2
Jason Johnston <jasonjoh@microsoft.com>
2016-02-02 14:50:46 -0500

    Added basic CRUD functions for messages
    Also added basic send mail functions
    
    - getMessage
    - createMessage
    - updateMessage
    - deleteMessage
    - sendNewMessage
    - sendDraftMessage

--- mail-api.js
+++ mail-api.js
@@ -8,7 +8,7 @@ module.exports = {
    * 
    * @param parameters {object} An object containing all of the relevant parameters. Possible values:
    * @param parameters.token {string} The access token.
-   * @param [parameters.useMe] {boolean} If true, use the /Me segement instead of the /Users/<email> segment. This parameter defaults to false and is ignored if the parameters.user.email parameter isn't provided (the /Me segement is always used in this case).
+   * @param [parameters.useMe] {boolean} If true, use the /Me segment instead of the /Users/<email> segment. This parameter defaults to false and is ignored if the parameters.user.email parameter isn't provided (the /Me segment is always used in this case).
    * @param [parameters.user.email] {string} The SMTP address of the user. If absent, the '/Me' segment is used in the API URL.
    * @param [parameters.user.timezone] {string} The timezone of the user.
    * @param [parameters.folderId] {string} The folder id. If absent, the API calls the `/User/Messages` endpoint. Valid values of this parameter are:
@@ -56,7 +56,285 @@ module.exports = {
         }
       }
     });
-  }
+  },
+  
+  /**
+   * Used to get a specific message.
+   * 
+   * @param parameters {object} An object containing all of the relevant parameters. Possible values:
+   * @param parameters.token {string} The access token.
+   * @param parameters.messageId {string} The Id of the message.
+   * @param [parameters.useMe] {boolean} If true, use the /Me segment instead of the /Users/<email> segment. This parameter defaults to false and is ignored if the parameters.user.email parameter isn't provided (the /Me segment is always used in this case).
+   * @param [parameters.user.email] {string} The SMTP address of the user. If absent, the '/Me' segment is used in the API URL.
+   * @param [parameters.user.timezone] {string} The timezone of the user.
+   * @param [parameters.odataParams] {object} An object containing key/value pairs representing OData query parameters. See [Use OData query parameters]{@link https://msdn.microsoft.com/office/office365/APi/complex-types-for-mail-contacts-calendar#UseODataqueryparameters} for details.
+   * @param [callback] {function} A callback function that is called when the function completes. It should have the signature `function (error, result)`.
+   */
+  getMessage: function(parameters, callback) {
+    var userSpec = utilities.getUserSegment(parameters);
+    
+    var requestUrl = base.apiEndpoint() + userSpec + '/Messages/' + parameters.messageId;
+    
+    var apiOptions = {
+      url: requestUrl,
+      token: parameters.token,
+      user: parameters.user
+    };
+    
+    if (parameters.odataParams !== undefined) {
+      apiOptions['query'] = parameters.odataParams;
+    }
+    
+    base.makeApiCall(apiOptions, function(error, response) {
+      if (error) {
+        if (typeof callback === 'function') {
+          callback(error, response);
+        }
+      }
+      else if (response.statusCode !== 200) {
+        if (typeof callback === 'function') {
+          callback('REST request returned ' + response.statusCode + '; body: ' + JSON.stringify(response.body), response);
+        }
+      }
+      else {
+        if (typeof callback === 'function') {
+          callback(null, response.body);
+        }
+      }
+    });
+  },
+  
+  /**
+   * Create a new message
+   * 
+   * @param parameters {object} An object containing all of the relevant parameters. Possible values:
+   * @param parameters.token {string} The access token.
+   * @param parameters.message {object}: The JSON-serializable message 
+   * @param [parameters.useMe] {boolean} If true, use the /Me segment instead of the /Users/<email> segment. This parameter defaults to false and is ignored if the parameters.user.email parameter isn't provided (the /Me segment is always used in this case).
+   * @param [parameters.user.email] {string} The SMTP address of the user. If absent, the '/Me' segment is used in the API URL.
+   * @param [parameters.user.timezone] {string} The timezone of the user.
+   * @param [parameters.folderId] {string} The folder id. If absent, the API calls the `/User/Messages` endpoint.
+   * @param [callback] {function} A callback function that is called when the function completes. It should have the signature `function (error, result)`.
+   */
+  createMessage: function(parameters, callback) {
+    var userSpec = utilities.getUserSegment(parameters);
+    var folderSpec = parameters.folderId === undefined ? '' : getFolderSegment() + parameters.folderId;
+    
+    var requestUrl = base.apiEndpoint() + userSpec + folderSpec + '/Messages';
+    
+    var apiOptions = {
+      url: requestUrl,
+      token: parameters.token,
+      user: parameters.user,
+      payload: parameters.message,
+      method: 'POST'
+    };
+    
+    base.makeApiCall(apiOptions, function(error, response) {
+      if (error) {
+        if (typeof callback === 'function') {
+          callback(error, response);
+        }
+      }
+      else if (response.statusCode !== 201) {
+        if (typeof callback === 'function') {
+          callback('REST request returned ' + response.statusCode + '; body: ' + JSON.stringify(response.body), response);
+        }
+      }
+      else {
+        if (typeof callback === 'function') {
+          callback(null, response.body);
+        }
+      }
+    });
+  },
+  
+  /**
+   * Update a specific message.
+   * 
+   * @param parameters {object} An object containing all of the relevant parameters. Possible values:
+   * @param parameters.token {string} The access token.
+   * @param parameters.messageId {string} The Id of the message.
+   * @param parameters.update {object}: The JSON-serializable update payload 
+   * @param [parameters.useMe] {boolean} If true, use the /Me segment instead of the /Users/<email> segment. This parameter defaults to false and is ignored if the parameters.user.email parameter isn't provided (the /Me segment is always used in this case).
+   * @param [parameters.user.email] {string} The SMTP address of the user. If absent, the '/Me' segment is used in the API URL.
+   * @param [parameters.user.timezone] {string} The timezone of the user.
+   * @param [parameters.odataParams] {object} An object containing key/value pairs representing OData query parameters. See [Use OData query parameters]{@link https://msdn.microsoft.com/office/office365/APi/complex-types-for-mail-contacts-calendar#UseODataqueryparameters} for details.
+   * @param [callback] {function} A callback function that is called when the function completes. It should have the signature `function (error, result)`.
+   */
+  updateMessage: function(parameters, callback) {
+    var userSpec = utilities.getUserSegment(parameters);
+    
+    var requestUrl = base.apiEndpoint() + userSpec + '/Messages/' + parameters.messageId;
+    
+    var apiOptions = {
+      url: requestUrl,
+      token: parameters.token,
+      user: parameters.user,
+      payload: parameters.update,
+      method: 'PATCH'
+    };
+    
+    if (parameters.odataParams !== undefined) {
+      apiOptions['query'] = parameters.odataParams;
+    }
+    
+    base.makeApiCall(apiOptions, function(error, response) {
+      if (error) {
+        if (typeof callback === 'function') {
+          callback(error, response);
+        }
+      }
+      else if (response.statusCode !== 200) {
+        if (typeof callback === 'function') {
+          callback('REST request returned ' + response.statusCode + '; body: ' + JSON.stringify(response.body), response);
+        }
+      }
+      else {
+        if (typeof callback === 'function') {
+          callback(null, response.body);
+        }
+      }
+    });
+  },
+  
+  /**
+   * Delete a specific message.
+   * 
+   * @param parameters {object} An object containing all of the relevant parameters. Possible values:
+   * @param parameters.token {string} The access token.
+   * @param parameters.messageId {string} The Id of the message.
+   * @param [parameters.useMe] {boolean} If true, use the /Me segment instead of the /Users/<email> segment. This parameter defaults to false and is ignored if the parameters.user.email parameter isn't provided (the /Me segment is always used in this case).
+   * @param [parameters.user.email] {string} The SMTP address of the user. If absent, the '/Me' segment is used in the API URL.
+   * @param [parameters.user.timezone] {string} The timezone of the user.
+   * @param [parameters.odataParams] {object} An object containing key/value pairs representing OData query parameters. See [Use OData query parameters]{@link https://msdn.microsoft.com/office/office365/APi/complex-types-for-mail-contacts-calendar#UseODataqueryparameters} for details.
+   * @param [callback] {function} A callback function that is called when the function completes. It should have the signature `function (error, result)`.
+   */
+  deleteMessage: function(parameters, callback) {
+    var userSpec = utilities.getUserSegment(parameters);
+    
+    var requestUrl = base.apiEndpoint() + userSpec + '/Messages/' + parameters.messageId;
+    
+    var apiOptions = {
+      url: requestUrl,
+      token: parameters.token,
+      user: parameters.user,
+      method: 'DELETE'
+    };
+    
+    if (parameters.odataParams !== undefined) {
+      apiOptions['query'] = parameters.odataParams;
+    }
+    
+    base.makeApiCall(apiOptions, function(error, response) {
+      if (error) {
+        if (typeof callback === 'function') {
+          callback(error, response);
+        }
+      }
+      else if (response.statusCode !== 204) {
+        if (typeof callback === 'function') {
+          callback('REST request returned ' + response.statusCode + '; body: ' + JSON.stringify(response.body), response);
+        }
+      }
+      else {
+        if (typeof callback === 'function') {
+          callback(null, response.body);
+        }
+      }
+    });
+  },
+  
+  /**
+   * Sends a new message
+   * 
+   * @param parameters {object} An object containing all of the relevant parameters. Possible values:
+   * @param parameters.token {string} The access token.
+   * @param parameters.message {object}: The JSON-serializable message 
+   * @param [parameters.saveToSentItems] {boolean}: Set to false to bypass saving a copy to the Sent Items folder. Default is true.
+   * @param [parameters.useMe] {boolean} If true, use the /Me segment instead of the /Users/<email> segment. This parameter defaults to false and is ignored if the parameters.user.email parameter isn't provided (the /Me segment is always used in this case).
+   * @param [parameters.user.email] {string} The SMTP address of the user. If absent, the '/Me' segment is used in the API URL.
+   * @param [parameters.user.timezone] {string} The timezone of the user.
+   * @param [callback] {function} A callback function that is called when the function completes. It should have the signature `function (error, result)`.
+   */
+  sendNewMessage: function(parameters, callback) {
+    var userSpec = utilities.getUserSegment(parameters);
+    
+    var requestUrl = base.apiEndpoint() + userSpec + '/sendmail';
+    
+    var payload = {
+      Message: parameters.message,
+      SaveToSentItems: parameters.saveToSentItems !== undefined ? parameters.saveToSentItems : 'true'
+    };
+    
+    var apiOptions = {
+      url: requestUrl,
+      token: parameters.token,
+      user: parameters.user,
+      payload: payload,
+      method: 'POST'
+    };
+    
+    base.makeApiCall(apiOptions, function(error, response) {
+      if (error) {
+        if (typeof callback === 'function') {
+          callback(error, response);
+        }
+      }
+      else if (response.statusCode !== 202) {
+        if (typeof callback === 'function') {
+          callback('REST request returned ' + response.statusCode + '; body: ' + JSON.stringify(response.body), response);
+        }
+      }
+      else {
+        if (typeof callback === 'function') {
+          callback(null, response.body);
+        }
+      }
+    });
+  },
+  
+  /**
+   * Sends a draft message.
+   * 
+   * @param parameters {object} An object containing all of the relevant parameters. Possible values:
+   * @param parameters.token {string} The access token.
+   * @param parameters.messageId {string} The Id of the message.
+   * @param [parameters.useMe] {boolean} If true, use the /Me segment instead of the /Users/<email> segment. This parameter defaults to false and is ignored if the parameters.user.email parameter isn't provided (the /Me segment is always used in this case).
+   * @param [parameters.user.email] {string} The SMTP address of the user. If absent, the '/Me' segment is used in the API URL.
+   * @param [parameters.user.timezone] {string} The timezone of the user.
+   * @param [callback] {function} A callback function that is called when the function completes. It should have the signature `function (error, result)`.
+   */
+  sendDraftMessage: function(parameters, callback) {
+    var userSpec = utilities.getUserSegment(parameters);
+    
+    var requestUrl = base.apiEndpoint() + userSpec + '/Messages/' + parameters.messageId + '/send';
+    
+    var apiOptions = {
+      url: requestUrl,
+      token: parameters.token,
+      user: parameters.user,
+      method: 'POST'
+    };
+    
+    base.makeApiCall(apiOptions, function(error, response) {
+      if (error) {
+        if (typeof callback === 'function') {
+          callback(error, response);
+        }
+      }
+      else if (response.statusCode !== 202) {
+        if (typeof callback === 'function') {
+          callback('REST request returned ' + response.statusCode + '; body: ' + JSON.stringify(response.body), response);
+        }
+      }
+      else {
+        if (typeof callback === 'function') {
+          callback(null, response.body);
+        }
+      }
+    });
+  },
 };
 
 /**
