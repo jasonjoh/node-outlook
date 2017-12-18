9f5d05b0f8a3d650721304182035c51c502098cb
Matt Lorey <mattlorey@gmail.com>
2017-12-18 10:23:59 -0700

    Add replyToMessage and replyToAllMessage functions

--- mail-api.js
+++ mail-api.js
@@ -757,6 +757,144 @@ module.exports = {
         }
       }
     });
+  },
+
+  /**
+   * Reply to sender
+   * 
+   * @param parameters {object} An object containing all of the relevant parameters. Possible values:
+   * @param parameters.token {string} The access token.
+   * @param parameters.messageId {string} The ID of the message to reply to.
+   * @param parameters.comment {string} The comment to include.  Can be an empty string.
+   * @param [parameters.useMe] {boolean} If true, use the `/Me` segment instead of the `/Users/<email>` segment. This parameter defaults to false and is ignored if the `parameters.user.email` parameter isn't provided (the `/Me` segment is always used in this case).
+   * @param [parameters.user.email] {string} The SMTP address of the user. If absent, the `/Me` segment is used in the API URL.
+   * @param [parameters.user.timezone] {string} The timezone of the user.
+   * @param [callback] {function} A callback function that is called when the function completes. It should have the signature `function (error, result)`.
+   * 
+   * @example var outlook = require('node-outlook');
+   * 
+   * // Set the API endpoint to use the v2.0 endpoint
+   * outlook.base.setApiEndpoint('https://outlook.office.com/api/v2.0');
+   * 
+   * // This is the oAuth token 
+   * var token = 'eyJ0eXAiOiJKV1Q...';
+   * 
+   * var comment = "Sounds great! See you tomorrow.";
+   * var messageId = "AAMkAGE0Mz8DmAAA=";
+   * 
+   * 
+   * outlook.mail.replyToMessage({token: token, comment: comment, messageId: messageId},
+   *   function(error, result){
+   *     if (error) {
+   *       console.log('replyToMessage returned an error: ' + error);
+   *     }
+   *     else if (result) {
+   *       console.log(JSON.stringify(result, null, 2));
+   *     }
+   *   });
+   */
+  replyToMessage: function(parameters, callback) {
+    var userSpec = utilities.getUserSegment(parameters);
+    
+    var requestUrl = base.apiEndpoint() + userSpec + '/messages/'+ parameters.messageId + '/reply' ;
+    
+    var payload = {
+      Comment: parameters.comment,
+    };
+    
+    var apiOptions = {
+      url: requestUrl,
+      token: parameters.token,
+      payload: payload,
+      method: 'POST'
+    };
+    
+    base.makeApiCall(apiOptions, function(error, response) {
+      if (error) {
+        if (typeof callback === 'function') {
+          callback(error, response);
+        }
+      }
+      else if (response.statusCode !== 202) {
+        if (typeof callback === 'function') {
+          callback('REST request returned ' + response.statusCode + '; body: ' + JSON.stringify(response.body), response);
+        }
+      }
+      else {
+        if (typeof callback === 'function') {
+          callback(null, response.body);
+        }
+      }
+    });
+  },
+
+  /**
+   * Reply to all
+   * 
+   * @param parameters {object} An object containing all of the relevant parameters. Possible values:
+   * @param parameters.token {string} The access token.
+   * @param parameters.messageId {string} The ID of the message to reply to.
+   * @param parameters.comment {string} The comment to include.  Can be an empty string.
+   * @param [parameters.useMe] {boolean} If true, use the `/Me` segment instead of the `/Users/<email>` segment. This parameter defaults to false and is ignored if the `parameters.user.email` parameter isn't provided (the `/Me` segment is always used in this case).
+   * @param [parameters.user.email] {string} The SMTP address of the user. If absent, the `/Me` segment is used in the API URL.
+   * @param [parameters.user.timezone] {string} The timezone of the user.
+   * @param [callback] {function} A callback function that is called when the function completes. It should have the signature `function (error, result)`.
+   * 
+   * @example var outlook = require('node-outlook');
+   * 
+   * // Set the API endpoint to use the v2.0 endpoint
+   * outlook.base.setApiEndpoint('https://outlook.office.com/api/v2.0');
+   * 
+   * // This is the oAuth token 
+   * var token = 'eyJ0eXAiOiJKV1Q...';
+   * 
+   * var comment = "Sounds great! See you tomorrow.";
+   * var messageId = "AAMkAGE0Mz8DmAAA=";
+   * 
+   * 
+   * outlook.mail.replyToAllMessage({token: token, comment: comment, messageId: messageId},
+   *   function(error, result){
+   *     if (error) {
+   *       console.log('replyToMessage returned an error: ' + error);
+   *     }
+   *     else if (result) {
+   *       console.log(JSON.stringify(result, null, 2));
+   *     }
+   *   });
+   */
+  replyToAllMessage: function(params, callback) {
+    var userSpec = utilities.getUserSegment(parameters);
+    
+    var requestUrl = base.apiEndpoint() + userSpec + '/messages/'+ parameters.messageId + '/replyall' ;
+    
+    var payload = {
+      Comment: parameters.comment,
+    };
+    
+    var apiOptions = {
+      url: requestUrl,
+      token: parameters.token,
+      payload: payload,
+      method: 'POST'
+    };
+    
+    base.makeApiCall(apiOptions, function(error, response) {
+      if (error) {
+        if (typeof callback === 'function') {
+          callback(error, response);
+        }
+      }
+      else if (response.statusCode !== 202) {
+        if (typeof callback === 'function') {
+          callback('REST request returned ' + response.statusCode + '; body: ' + JSON.stringify(response.body), response);
+        }
+      }
+      else {
+        if (typeof callback === 'function') {
+          callback(null, response.body);
+        }
+      }
+    });
   }
 };
 
